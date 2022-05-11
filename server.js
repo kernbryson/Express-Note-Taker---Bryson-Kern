1b9c477e05db6a3619922256a68e7a3d508a8bcc
Bryson Kern <kernbryson@yahoo.com>
2022-05-11 13:36:31 -0400

    created array for title and text

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
-const savedNotes = require('./db/db.json');
+const savedNotes = require("./db/db.json");
+const req = require("express/lib/request");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get('/api/notes', (req, res) => {
-    res.json(savedNotes.slice(1));
-  
-    console.info(`${req.method} request received to get reviews`);
-  });
+app.get("/api/notes", (req, res) => {
+  res.json(savedNotes.slice(1));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+  console.info(`${req.method} request received to get reviews`);
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a review`);
+  console.log(req.body);
+  const { title, text } = req.body;
+  if(title && text){
+      const newNote = {
+          title,
+          text
+      }
+      fs.readFile('')
+  }
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-function 
